232f7215bf407f5f300fe3f9db81cd7079ceba44
fiddyspence <chris@spence.org.uk>
2012-08-31 11:05:01 +0100

    keep those commits coming

--- puppet/reports/mconotify.rb
+++ puppet/reports/mconotify.rb
@@ -19,13 +19,14 @@ Puppet::Reports.register_report(:mconotify) do
   end
 
   desc <<-DESC
-Send notification of failed reports to an XMPP user or MUC.
+Orchestrate puppet runs via mcollective on changed resources in classes, or on particular nodes
 DESC
 
   def process
-      a=File.open("/tmp/shitt#{Time.now.to_i}",'w') if MCO_DEBUG
-      a.write "#{MCO_CONFIG}, #{MCO_TIMEOUT}\n" if MCO_DEBUG 
-      if self.status == 'changed' 
+    a=File.open("/tmp/#{self.host}#{Time.now.to_i}",'w') if MCO_DEBUG
+    a.write "MCONOTIFY CONFIG:\n\n#{config}\n" if MCO_DEBUG
+    a.write self.resource_statuses if MCO_DEBUG
+    if self.status == 'changed' 
       svcs = MCollective::RPC::Client.new("puppetd", :configfile => MCO_CONFIG, :options => {:verbose=>false, :progress_bar=>false , :timeout=> MCO_TIMEOUT, :mcollective_limit_targets=>false, :config=> MCO_CONFIG, :filter=>{"cf_class"=>[], "agent"=>["puppetd"], "identity"=>[], "fact"=>[]}, :collective=>nil, :disctimeout=>2} )
       svcs.progress = false
       svcs.runonce(:forcerun=> "true")
