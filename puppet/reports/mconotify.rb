2ddf1c0df5d9dc33f625ae52e90b919dffed5f10
fiddyspence <chris@spence.org.uk>
2012-08-31 10:52:25 +0100

    added debug entries:

--- puppet/reports/mconotify.rb
+++ puppet/reports/mconotify.rb
@@ -12,6 +12,7 @@ Puppet::Reports.register_report(:mconotify) do
   MCO_TIMEOUT = config[:mcotimeout] || 5
   MCO_CLASSES = config[:classes]
   MCO_NODES = config[:nodes] 
+  MCO_DEBUG = config[:debug] || false
 
   if ! MCO_CLASSES and ! MCO_NODES
     raise(Puppet::ParseError, "mconotify: Either no classes or no nodes to notify on specified")
@@ -22,14 +23,14 @@ Send notification of failed reports to an XMPP user or MUC.
 DESC
 
   def process
-      a=File.open("/tmp/shitt#{Time.now.to_i}",'w')
-      a.write "#{MCO_CONFIG}, #{MCO_TIMEOUT}\n"
+      a=File.open("/tmp/shitt#{Time.now.to_i}",'w') if MCO_DEBUG
+      a.write "#{MCO_CONFIG}, #{MCO_TIMEOUT}\n" if MCO_DEBUG 
       if self.status == 'changed' 
       svcs = MCollective::RPC::Client.new("puppetd", :configfile => MCO_CONFIG, :options => {:verbose=>false, :progress_bar=>false , :timeout=> MCO_TIMEOUT, :mcollective_limit_targets=>false, :config=> MCO_CONFIG, :filter=>{"cf_class"=>[], "agent"=>["puppetd"], "identity"=>[], "fact"=>[]}, :collective=>nil, :disctimeout=>2} )
       svcs.progress = false
       svcs.runonce(:forcerun=> "true")
       svcs.disconnect
-      a.write self.resource_statuses
+      a.write self.resource_statuses if MCO_DEBUG
     end
   end
 end
