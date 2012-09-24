ff2a2eec7d8175e2210d1ba4a32defc76447ce13
fiddyspence <chris@spence.org.uk>
2012-09-24 16:23:55 +0100

    changed to 2 difference mco invocations - one for class, one for node

--- puppet/reports/mconotify.rb
+++ puppet/reports/mconotify.rb
@@ -20,7 +20,7 @@ DESC
 
   def process
     notifystuff = []
-    Puppet.notice "MCONOTIFY: CONFIG:#{CONFIG.inspect}\n\n" if MCO_DEBUG
+    Puppet.notice "MCONOTIFY #{self.name}: CONFIG:#{CONFIG.inspect}\n\n" if MCO_DEBUG
 
     if self.status == 'changed' 
       begin
@@ -30,12 +30,12 @@ DESC
             matching_tags.each do |tag|
               notifystuff << tag unless notifystuff.member?(tag)
             end
-            Puppet.notice "MCONOTIFY: Added mconotify tag #{matching_tags.class}"
+            Puppet.notice "MCONOTIFY #{self.name}: Added mconotify tag #{matching_tags}"
           end
         end
-        Puppet.notice "MCONOTIFY: End of tag matching\n" if MCO_DEBUG
+        Puppet.notice "MCONOTIFY #{self.name}: End of tag matching\n" if MCO_DEBUG
       rescue => cow
-        Puppet.notice "MCONOTIFY: couldn't output resource statuses #{cow}" if MCO_DEBUG
+        Puppet.notice "MCONOTIFY #{self.name}: couldn't output resource statuses #{cow}" if MCO_DEBUG
       end
 
       factfilter=[]
@@ -44,20 +44,35 @@ DESC
 
       notifystuff.each do |filter|
         if filter.to_s =~ /:-node:-/
-          Puppet.notice "MCONOTIFY: matched #{filter} to a node\n"  if MCO_DEBUG
-          nodefilter << "/#{filter.to_s.split(':-')[2]}/"  if MCO_DEBUG
+          Puppet.notice "MCONOTIFY #{self.name}: matched #{filter} to a node\n"  if MCO_DEBUG
+          nodefilter << "#{filter.to_s.split(':-')[2]}"  if MCO_DEBUG
         end
         if filter.to_s =~ /:-class:-/
-          Puppet.notice "MCONOTIFY: matched #{filter} to a class\n"  if MCO_DEBUG
-          classfilter << "/#{filter.to_s.split(':-')[2]}/"  if MCO_DEBUG
+          Puppet.notice "MCONOTIFY #{self.name}: matched #{filter} to a class\n"  if MCO_DEBUG
+          classfilter << "#{filter.to_s.split(':-')[2]}"  if MCO_DEBUG
         end
       end
 
-      Puppet.notice "MCONOTIFY: Filters: node #{nodefilter.count} class #{classfilter.count}\n"  if MCO_DEBUG
+      Puppet.notice "MCONOTIFY #{self.name}: Filters: node #{nodefilter.count} class #{classfilter.count}\n"  if MCO_DEBUG
 
-      if nodefilter.count > 0 or classfilter.count > 0
-        Puppet.notice "MCONOTIFY: Doing an mco run for #{nodefilter} and #{classfilter}\n"  if MCO_DEBUG
-        svcs = MCollective::RPC::Client.new("puppetd", :configfile => MCO_CONFIG, :options => {:verbose=>false, :progress_bar=>false , :timeout=> MCO_TIMEOUT, :mcollective_limit_targets=>false, :config=> MCO_CONFIG, :filter=>{"cf_class"=>classfilter.flatten, "agent"=>["puppetd"], "identity"=>nodefilter.flatten, "fact"=>factfilter}, :collective=>MCO_COLLECTIVE, :disctimeout=>2} )
+      if nodefilter.count > 0
+
+        Puppet.notice "MCONOTIFY #{self.name}: Doing an mco run for #{nodefilter}"  if MCO_DEBUG
+        thefilter="/#{nodefilter.join('|')}/"
+        Puppet.notice "MCONOTIFY #{self.name}: #{thefilter}"
+        svcs = MCollective::RPC::Client.new("puppetd", :configfile => MCO_CONFIG, :options => {:verbose=>false, :progress_bar=>false , :timeout=> MCO_TIMEOUT, :mcollective_limit_targets=>false, :config=> MCO_CONFIG, :filter=>{"cf_class"=>[], "agent"=>["puppetd"], "identity"=>[thefilter], "fact"=>factfilter}, :collective=>MCO_COLLECTIVE, :disctimeout=>2} )
+
+        svcs.runonce(:forcerun=> true)
+        svcs.disconnect
+
+      end
+
+      if classfilter.count > 0
+
+        Puppet.notice "MCONOTIFY #{self.name}: Doing an mco run for #{classfilter}"  if MCO_DEBUG
+        thefilter="/#{classfilter.join('|')}/"
+
+        svcs = MCollective::RPC::Client.new("puppetd", :configfile => MCO_CONFIG, :options => {:verbose=>false, :progress_bar=>false , :timeout=> MCO_TIMEOUT, :mcollective_limit_targets=>false, :config=> MCO_CONFIG, :filter=>{"cf_class"=>[thefilter], "agent"=>["puppetd"], "identity"=>[], "fact"=>factfilter}, :collective=>MCO_COLLECTIVE, :disctimeout=>2} )
 
         svcs.runonce(:forcerun=> true)
         svcs.disconnect
