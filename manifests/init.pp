b9bb1236e757c8b0b85971919219dfb7e72e50d6
fiddyspence <chris.spence@puppetlabs.com>
2013-10-29 09:25:43 +0000

    uprevving class to support template for writing processor config

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,26 @@
-class mconotify {
+ mconotify (
+  $mco_clientcfg = '/etc/puppetlabs/puppet/client.cfg',
+  $mco_clientcfg = 5,
+  $mco_debug = 'false',
+  $mco_delimiter = '--',
+  $configfromtemplate = 'false',
+){
+
+  $template = str2bool($configfromtemplate)
 
   file { "${::puppet_confdir}/mconotify.yaml":
-    ensure => present,
-    source => "puppet:///modules/${module_name}/mconotify.yaml",
-    owner  => '0',
-    group  => '0',
-    mode   => '0644',
+    ensure  => present,
+    source  => $template ? {
+                 false => "puppet:///modules/${module_name}/mconotify.yaml",
+                 true  => undef
+               },
+    content => $template ? {
+                 false => undef,
+                 true  => template('mconotify/mconotify.yaml.erb')
+               },
+    owner   => '0',
+    group   => '0',
+    mode    => '0644',
   }
 
 }
